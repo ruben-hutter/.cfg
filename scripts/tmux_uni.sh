b0f241c607383cfae44adf9205cb7b5c04c4a540
ruben-hutter <hutter.ruben@gmail.com>
2024-04-08 20:55:01 +0200

    Update tmux script

--- scripts/tmux_uni.sh
+++ scripts/tmux_uni.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if a tmux session "uni" exists
-# If it does, attach to it
-# If it doesn't, create it from ~/UNI/
-
 if tmux has-session -t uni 2>/dev/null; then
     tmux attach -t uni
 else
